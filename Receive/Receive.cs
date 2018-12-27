76a7709cb9ee57c2bac312e42b0f4e30deb7f5ad
VictorC <ubfacility@gmail.com>
2018-12-27 13:35:38 +0100

    trying out rabbitMQ in .net core

---
+++ Receive/Receive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive {
+    class Program {
+        public static void Main (string[] args) {
+            var factory = new ConnectionFactory () { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection ())
+
+            using (var channel = connection.CreateModel ()) {
+                channel.QueueDeclare (queue: "Hello",
+                    durable : false,
+                    exclusive : false,
+                    autoDelete : false,
+                    arguments : null);
+                var consumer = new EventingBasicConsumer (channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString (body);
+                    Console.WriteLine (" [x] Received {0}", message);
+                };
+                channel.BasicConsume (queue: "Hello",
+                    autoAck : true,
+                    consumer : consumer);
+                Console.WriteLine (" Press [enter] to exit.");
+                Console.ReadLine ();
+            }
+
+        }
+    }
+}
